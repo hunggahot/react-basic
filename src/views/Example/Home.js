f0cae2e5d099dea69fb4cf43b3ca1cb4d888515c
hunggahot <hunggahot@gmail.com>
2023-06-27 00:00:18 +0700

    Links & Nav Links

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router";
 
 class Home extends React.Component {
 
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        }, 3000)
+    }
+
     render() {
+        console.log('>>> check props: ', this.props)
         return (
             <div>
                 Welcome Homepage!
@@ -11,4 +19,4 @@ class Home extends React.Component {
     }
 }
 
-export default Home
+export default withRouter(Home);
